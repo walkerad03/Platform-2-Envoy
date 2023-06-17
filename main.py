571b108a5abd191d199cea05dc63fc8569bae5ef
Walker Davis <walker@wkeithdavis.com>
2023-06-17 18:38:39 -0400

    Added FON column support

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ envoy_import["Suffix"] = crm_import["Suffix"]
 envoy_import["Mailing Name"] = crm_import["Salutation"]
 envoy_import["Email"] = crm_import["Email"]
 envoy_import["Phone Number"] = crm_import["Mobile"]
-envoy_import["Secondary Phone Number"] = crm_import["Phone"]
+envoy_import["Second Phone Number"] = crm_import["Phone"]
 envoy_import["Address 1"] = crm_import["Address1"]
 envoy_import["Address 2"] = crm_import["Address2"]
 envoy_import["City"] = crm_import["City"]
@@ -20,10 +20,10 @@ envoy_import["State"] = crm_import["State"]
 envoy_import["Zip Code"] = crm_import["Zip"]
 envoy_import["Birth Month"] = crm_import["Birthday"].str.extract(r'-(\d{2})-', expand=False)
 envoy_import["Birth Day"] = crm_import["Birthday"].str.extract(r'-(\d{2})$', expand=False)
-envoy_import["Anniversary"] = pd.to_datetime(crm_import["Closing Anniversary"]).dt.strftime('%m/%d/%Y')
+envoy_import["Anniversary"] = pd.to_datetime(crm_import["Closing Anniversary"], format='').dt.strftime('%m/%d/%Y')
 envoy_import["Pronouns"] = crm_import["Pronouns"]
+envoy_import["FON"] = crm_import["Custom Farm: FON"]
 
-# FON
 # Farms
 
 envoy_import["Rank"] = crm_import["Rank"]
