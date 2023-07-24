d54efc7cac25f06468983f541701c1b7b98d8acc
Walker Davis <wdavis@unc.edu>
2023-07-24 13:02:33 -0400

    Added agent column check

--- converter.py
+++ converter.py
@@ -93,7 +93,8 @@ def convert_to_envoy(filename: str, output_filename: str) -> None:
 
     init_row_count = envoy_import.shape[0]
     # Drop columns where agent is listed as co-op agent.
-    envoy_import = envoy_import.drop(crm_import[crm_import.Agent == "X"].index)
+    if ("Agent" in crm_import.columns):
+        envoy_import = envoy_import.drop(crm_import[crm_import.Agent == "X"].index)
 
     postop_row_count = envoy_import.shape[0]
     print(f"Rows removed: {init_row_count - postop_row_count}")
